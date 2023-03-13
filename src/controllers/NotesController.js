5d3cb33e4575bc549b6c316aec78d29b871c2f11
joao-milhomem <milhomemjoao.work@gmail.com>
2023-03-13 15:53:42 -0300

    Correção de bugs

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -51,27 +51,16 @@ class NotesController {
   }
 
   async index(request, response) {
-    const { title, tags } = request.query;
+    const { title } = request.query;
     const user_id = request.user.id;
-    let notes;
 
-    if (tags) {
-      const listTags = tags.split(",").map((tag) => tag.trim());
-      notes = await knex("tags")
-        .select(["notes.id", "notes.title", "notes.user_id"])
-        .where("notes.user_id", user_id)
-        .whereLike("notes.title", `%${title}%`)
-        .whereIn("tags.name", listTags)
-        .innerJoin("notes", "notes.id", "tags.note_id")
-        .orderBy("notes.title");
-    } else {
-      notes = await knex("notes")
-        .where({ user_id })
-        .whereLike("title", `%${title}%`)
-        .orderBy("title");
-    }
+    const notes = await knex("notes")
+      .where({ user_id })
+      .whereLike("title", `%${title}%`)
+      .orderBy("title");
 
     const allUserTags = await knex("tags").where({ user_id });
+
     const notesWithTags = notes.map((note) => {
       const filteredTags = allUserTags.filter((tag) => tag.note_id === note.id);
       return {
