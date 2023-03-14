25a53411c06c9e76f63f80195fb8d29035786b1e
joao-milhomem <milhomemjoao.work@gmail.com>
2023-03-14 12:19:41 -0300

    Alterando config do pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: "app1",
+      name: "app",
       script: "./src/server.js",
+      instances: "max",
+      env: {
+        NODE_ENV: "development",
+      },
       env_production: {
         NODE_ENV: "production",
-      },
-      env_development: {
-        NODE_ENV: "development",
       },
     },
   ],
